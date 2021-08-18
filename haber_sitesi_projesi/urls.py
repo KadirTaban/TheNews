03db6ab90a5931d563ae0336752d990edfd912bb
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-08-18 20:06:10 +0300

    show image

--- haber_sitesi_projesi/urls.py
+++ haber_sitesi_projesi/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # new
+from django.conf.urls.static import static # new
 
 
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
 
 
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
